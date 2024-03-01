0f340a109c7ff54d33570d4aa94988b1a4cf5f0c
hyungjin <skrt1533@gmail.com>
2024-03-01 23:54:01 +0900

    before zzz

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,8 +1,7 @@
-import 'dart:js_util';
-import 'dart:ui_web';
 
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
 
@@ -31,7 +30,7 @@ class WorldTime {
       now = now.add(Duration(hours:int.parse(offset)));
 
       //set the time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     }
     catch(e) {
       print('caught error: $e');
